6b6db9c674c6d2a5eaee538579a16d09c1ea52f7
js07 <19861096+js07@users.noreply.github.com>
2021-09-15 13:19:01 -0400

    Add fileUrl props to G_D replace file

--- components/google_drive/actions/replace-file/replace-file.js
+++ components/google_drive/actions/replace-file/replace-file.js
@@ -1,13 +1,14 @@
 const fs = require("fs");
 const googleDrive = require("../../google_drive.app");
 const common = require("../common.js");
+const axios = require("axios");
 
 module.exports = {
   ...common,
   key: "google_drive-replace-file",
   name: "Replace File",
   description: "Upload a file that replaces an existing file",
-  version: "0.0.5",
+  version: "0.0.10",
   type: "action",
   props: {
     googleDrive,
@@ -38,26 +39,47 @@ module.exports = {
         return this.googleDrive.listFilesOptions(nextPageToken, opts);
       },
     },
-    pathToFile: {
+    fileUrl: {
+      type: "string",
+      label: "File URL",
+      description:
+        "The URL of the file to attach. Must specify either File URL or File Path.",
+      optional: true,
+      default: "",
+    },
+    filePath: {
       type: "string",
       label: "File path",
       description:
-        "The path to the file saved to the /tmp, e.g. /tmp/myFile.csv .",
-      optional: false,
+        "The path to the file saved to the /tmp, e.g. /tmp/myFile.csv . Must specify either File URL or File Path.",
+      optional: true,
+      default: "",
     },
   },
   methods: {
     ...common.methods,
   },
   async run() {
+    if (!this.fileUrl && !this.filePath) {
+      throw new Error("One of File URL and File Path is required.");
+    }
     const drive = this.googleDrive.drive();
+    const file = this.fileUrl
+      ? (
+        await axios({
+          url: this.fileUrl,
+          method: "GET",
+          responseType: "stream",
+        })
+      ).data
+      : fs.createReadStream(this.filePath);
     return (
       await drive.files.update({
         fileId: this.fileId,
         media: {
           mimeType: this.filetype,
           uploadType: "media",
-          body: fs.createReadStream(this.pathToFile),
+          body: file,
         },
       })
     ).data;
