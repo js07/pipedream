b9b6f84c7714f5cb2d5ec77279b3d43ded7e8e4b
js07 <19861096+js07@users.noreply.github.com>
2021-09-28 15:51:09 -0400

    Implement `autoPaging` prop in Telegram actions
    Actions with `autoPaging` prop: `list-chat`,`list-updates`

--- components/telegram/actions/list-chats/list-chats.js
+++ components/telegram/actions/list-chats/list-chats.js
@@ -4,7 +4,7 @@ module.exports = {
   key: "telegram-list-chats",
   name: "List Chats",
   description: "List available Telegram chats",
-  version: "0.0.6",
+  version: "0.0.7",
   type: "action",
   props: {
     telegram,
@@ -48,6 +48,16 @@ module.exports = {
       offset: this.offset,
       limit: this.limit,
     });
+    if (this.autoPaging && updates.length > 0) {
+      const lastUpdateId = updates[updates.length - 1].update_id;
+      // "Confirm" updates by calling API to get updates using an offset of the
+      // last `update_id + 1` See [documentation for getUpdates in the Telegram
+      // Bot API reference](https://core.telegram.org/bots/api#getupdates)
+      await this.telegram.getUpdates({
+        offset: lastUpdateId + 1,
+        limit: 1,
+      });
+    }
     return updates.map(this.generateResultFromUpdate);
   },
 };
--- components/telegram/actions/list-updates/list-updates.js
+++ components/telegram/actions/list-updates/list-updates.js
@@ -4,7 +4,7 @@ module.exports = {
   key: "telegram-list-updates",
   name: "List Updates",
   description: "Retrieves a list of updates from the Telegram server",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
   props: {
     telegram,
@@ -28,9 +28,20 @@ module.exports = {
     },
   },
   async run() {
-    return await this.telegram.getUpdates({
+    const updates = await this.telegram.getUpdates({
       offset: this.offset,
       limit: this.limit,
     });
+    if (this.autoPaging && updates.length > 0) {
+      const lastUpdateId = updates[updates.length - 1].update_id;
+      // "Confirm" updates by calling API to get updates using an offset of the
+      // last `update_id + 1` See [documentation for getUpdates in the Telegram
+      // Bot API reference](https://core.telegram.org/bots/api#getupdates)
+      await this.telegram.getUpdates({
+        offset: lastUpdateId + 1,
+        limit: 1,
+      });
+    }
+    return updates;
   },
 };
